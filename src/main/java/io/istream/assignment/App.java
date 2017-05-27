e044801c1d50ddbdaaa2c88a0553272653a40f43
Kapoor <bmwi@M-C02T70E8GTF1.nordstrom.net>
2017-05-27 00:13:28 -0700

    Formatting

--- src/main/java/io/istream/assignment/App.java
+++ src/main/java/io/istream/assignment/App.java
@@ -10,6 +10,7 @@ public class App {
 
     private static final Character TEXT_DELIMITER = ' ';
     private static final Set<Character> SENTENCE_SEPARATORS = new HashSet<>();
+
     static {
         SENTENCE_SEPARATORS.add(',');
         SENTENCE_SEPARATORS.add('.');
@@ -17,9 +18,10 @@ public class App {
     }
 
     public static void main(String[] args) {
-        if(args.length > 0) {
-            for(int index = 0; index < args.length; index++) {
-                System.out.println("For String at Index : " + index + " Most frequent word : " + getMostFrequentWordUsingMap(args[index]));
+        if (args.length > 0) {
+            for (int index = 0; index < args.length; index++) {
+                System.out.println("For String at Index : " + index +
+                        " Most frequent word : " + getMostFrequentWordUsingMap(args[index]));
             }
         } else {
             throw new RuntimeException("No args provided.");
@@ -29,19 +31,20 @@ public class App {
 
 
     /**
-     Time and Space complexity of this function is
-     Time:- O(n+n)=O(n)
-     Space :- O(n)
+     * Time and Space complexity of this function is
+     * Time:- O(n+n)=O(n)
+     * Space :- O(n)
      */
     public static String getMostFrequentWordUsingMap(String text) {
 
-        if(null!= text) {
+        if (null != text) {
             String mostFrequentWord = "";
             Integer max_frequency = 0;
             Map<String, Integer> wordFrequencyMap = getMap(text);
             Set<String> words = wordFrequencyMap.keySet();
 
-            //Time Complexity in iterating over Map of n words is also O(n) where key look up is constant time look up based on HashCode
+            //Time Complexity in iterating over Map of n words is also O(n) where key look up is constant time look up
+            // based on HashCode
             for (String word : words) {
                 Integer frequency = wordFrequencyMap.get(word);
                 if (max_frequency.compareTo(frequency) == -1) {
@@ -57,13 +60,15 @@ public class App {
     }
 
 
-    /** Constructing Map has Time Complexity O(n) and space complexity of  O(n) where n is number of words in input String */
+    /**
+     * Constructing Map has Time Complexity O(n) and Space complexity of O(n) where n is number of words in input Text
+     */
     private static Map<String, Integer> getMap(String text) {
         final Map<String, Integer> wordFrequencyMap = new HashMap<>();
         StringBuilder sb = new StringBuilder();
         for (char ch : text.toCharArray()) {
             if (TEXT_DELIMITER != ch) {
-                if(!SENTENCE_SEPARATORS.contains(ch)) {
+                if (!SENTENCE_SEPARATORS.contains(ch)) {
                     sb.append(ch);
                 }
             } else {
@@ -74,13 +79,13 @@ public class App {
         }
         //Add the Last word
         String word = sb.toString().trim();
-        if(!word.isEmpty()) {
+        if (!word.isEmpty()) {
             setMap(wordFrequencyMap, word);
         }
         return wordFrequencyMap;
     }
 
-    private static void setMap(Map<String, Integer> wordFrequencyMap , String word) {
+    private static void setMap(Map<String, Integer> wordFrequencyMap, String word) {
         Integer frequency = wordFrequencyMap.get(word);
         if (frequency == null) {
             wordFrequencyMap.put(word, 1);
